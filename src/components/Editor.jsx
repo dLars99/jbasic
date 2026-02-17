e8c77407c423e0cc35fe85e8224ea73749ba18a3
David Larsen <davidplarsen@gmail.com>
2026-02-17 06:45:42 -0600

    style editor window

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 export default function Editor({ value, onChange }) {
   return (
-    <textarea
-      className="editor"
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-      spellCheck={false}
-    />
+    <div className="editor-wrap">
+      <textarea
+        className="editor"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        spellCheck={false}
+      />
+    </div>
   );
 }
